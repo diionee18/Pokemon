c3cbbd3d53856821ef5ff96df281a66cfbd746c7
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-02-23 23:18:28 +0100

    La till animation

--- lag.js
+++ lag.js
@@ -2,9 +2,9 @@ const lagSpelare = () =>{
   let antalSpelare = document.querySelector("#minst-antal-spelare")
   let pokeCounter = document.querySelectorAll(".wrapper2 .pokemon-container").length
   if(pokeCounter < 3){
-    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span>Du behöver ${3- pokeCounter} till för att kunna spela `
+    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span>För att kunna spela behöver du ${3- pokeCounter} till`
   }else{
-    antalSpelare.innerHTML = `Du har ${pokeCounter} spelare, <br> du kan lägga till fler i reserv`
+    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span> du kan lägga till fler i reserv`
   }
 }
 
