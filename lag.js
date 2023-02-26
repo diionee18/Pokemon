8b308736aeeed4ecc377cc808ff61d6ff1bb9d09
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-02-26 11:24:54 +0100

    Css responsiv

--- lag.js
+++ lag.js
@@ -2,7 +2,7 @@ const lagSpelare = () =>{
   let antalSpelare = document.querySelector("#minst-antal-spelare")
   let pokeCounter = document.querySelectorAll(".wrapper2 .pokemon-container").length
   if(pokeCounter < 3){
-    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span>För att kunna spela behöver du ${3- pokeCounter} till`
+    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span>För att kunna spela behöver du minst ${3- pokeCounter} till`
     antalSpelare.style.border = "solid red 5px";
   }else{
     antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span> du kan lägga till fler i reserv`
