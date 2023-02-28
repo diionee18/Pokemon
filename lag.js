16768c160639ecf94aabadc86cb94f6672b353f1
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-02-28 19:27:39 +0100

    .

--- lag.js
+++ lag.js
@@ -4,11 +4,11 @@ const lagSpelare = () =>{
   let pokeCounter = document.querySelectorAll(".wrapper2 .pokemon-container").length
   antalSpelare.setAttribute("class","notis-section")
   if(pokeCounter < 3){
-    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémons <br>  </span>För att kunna spela behöver du minst ${3- pokeCounter} till`
+    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} Pokémon <br>  </span>För att kunna spela behöver du minst ${3- pokeCounter} till`
     antalSpelare.style.border = "solid red 5px";
     antalSpelare.style.padding = "0em 2em"
   }else{
-    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} ordinare Pokémons <br>  </span> och du har <span class="lagets-spelare" > ${antalReserv} </span> i reserv du kan lägga till fler `
+    antalSpelare.innerHTML = `<span class="lagets-spelare"> Du har ${pokeCounter} ordinare Pokémon <br>  </span> och du har <span class="lagets-spelare" > ${antalReserv} </span> i reserv, du kan lägga till fler `
     antalSpelare.style.border = "solid green 5px";
   }
 }
